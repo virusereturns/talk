f500c3b99e6909966c43385cc6d26b931273b6ec
nick-funk <nick.funk@outlook.com>
2021-08-24 13:29:04 -0600

    Update create/reply mutations to return story url in optimistic response

--- src/core/client/stream/tabs/Comments/Stream/PostCommentForm/CreateCommentMutation.ts
+++ src/core/client/stream/tabs/Comments/Stream/PostCommentForm/CreateCommentMutation.ts
@@ -152,6 +152,7 @@ graphql`
 graphql`
   fragment CreateCommentMutation_story on Story {
     id
+    url
     viewerRating {
       id
       tags {
@@ -336,6 +337,7 @@ export const CreateCommentMutation = createMutation(
                   },
                   story: {
                     id: input.storyID,
+                    url: story.url,
                     settings: {
                       moderation: storySettings.moderation,
                       mode: storySettings.mode,
